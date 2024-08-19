98dfbc8c2a692337ff84cf37c70fa758044f27da
ChoriyevMirzohid <mirzohid.ch@gmail.com>
2024-08-19 13:55:39 +0500

    commit

--- src/main/java/com/example/jwt_example/model/UserPrincipal.java
+++ src/main/java/com/example/jwt_example/model/UserPrincipal.java
@@ -1,6 +1,5 @@
 package com.example.jwt_example.model;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
